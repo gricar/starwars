bfa3d3732ff7de0dc5974a501348d45269b69fdb
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-11-11 15:59:20 -0300

    Updates

---
+++ precache-manifest.d1d22a4877469546db42ae16d59241b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "672e66d8c2d9d2a9a4f0d6c6f6a674de",
+    "url": "/starwars-planets/index.html"
+  },
+  {
+    "revision": "01df34bf2a51585f2d94",
+    "url": "/starwars-planets/static/js/2.c286563e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starwars-planets/static/js/2.c286563e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa0905dc88dd3a75923",
+    "url": "/starwars-planets/static/js/main.6f7ac680.chunk.js"
+  },
+  {
+    "revision": "63af275750f9e805b0aa",
+    "url": "/starwars-planets/static/js/runtime-main.f516939e.js"
+  }
+]);
